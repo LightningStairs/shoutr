ca64dd68994701d61642caefad92eef1d247e37a
Jenah <jenah.blitz@simplybusiness.com>
2018-11-24 17:00:25 -0500

    extract avatar method into its own module

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,16 +1,4 @@
-module ShoutHelper
-  def avatar(user)
-    image_tag gravatar_url(user.email)
-  end
-
-  def email_digest(email)
-    Digest::MD5.hexdigest(email)
-  end
-
-  def gravatar_url(email)
-    "https://www.gravatar.com/avatar/#{email_digest(email)}"
-  end
-
+module ShoutHelper 
   def like_button(shout)
     if current_user.liked?(shout)
       link_to "Unlike", unlike_shout_path(shout), method: :delete
