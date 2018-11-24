dd4ea4d537f4d1784897070e290e769b62c93a9b
Jenah <jenah.blitz@simplybusiness.com>
2018-11-24 13:16:38 -0500

    Update controller and partials to use new relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181026194228) do
+ActiveRecord::Schema.define(version: 20181124173125) do
 
   create_table "shouts", force: :cascade do |t|
-    t.string "body", null: false
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "content_type"
+    t.integer "content_id"
+    t.index ["content_id", "content_type"], name: "index_shouts_on_content_id_and_content_type"
     t.index ["user_id"], name: "index_shouts_on_user_id"
+  end
+
+  create_table "text_shouts", force: :cascade do |t|
+    t.string "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
